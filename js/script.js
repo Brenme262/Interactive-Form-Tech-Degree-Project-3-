55db9de1048ad54e0887b8b7ea11157414bb4a48
Brenme262 <brenden.messerschmidt@gmail.com>
2019-03-15 17:06:50 -0700

    Class selector Functioning
    Class Selector is up and running, it will disable conflickting classes and update a running tally of the cost of the selected classes.

--- js/script.js
+++ js/script.js
@@ -49,3 +49,51 @@ else {
 })
 
 ///$('#color').children().each(function(){console.log($(this).text())});
+let checkbox;
+let cost =0;
+
+
+$('.activities label').on('change', function(){
+  cost = 0;
+  if ($(this).text().indexOf('$100') >= 0){
+    checkbox = $(this).text().match(/\w+\s\d{1,2}\w{2}[-]\d{1,2}\w{2}/).slice(0).toString();
+
+//This if statement checks to see if the checkbox ends unchecked, if so it
+//sets the 'disabled' attribute on all matching items to 'false'. It also removes
+//the 'disabled' class from the label element.
+  if($(this).children().prop('checked')===false){
+    $('.activities').children('label').each(function(){
+      if ($(this).text().indexOf(checkbox) >= 0){
+          $(this).children().prop('disabled', false);
+          $(this).removeClass('disabled')
+      }
+    })
+  }
+//The else section sets the 'disabled' attribute to 'true' on any matching input
+//items other than the one that was selected. It also adds the 'disabled' class
+//to the element.
+  else{
+    $('.activities').children('label').each(function(){
+      if ($(this).text().indexOf(checkbox) >= 0 && $(this).children().prop('checked')===false){
+          $(this).children().prop('disabled', true);
+          $(this).addClass('disabled')
+      }
+    })
+  }
+}
+  $('.activities').children().each(function(){
+      if($(this).children().prop('checked')===true){
+        cost += parseInt($(this).text().match(/\d{3}/).slice(0).toString());
+
+      }
+  })
+
+$('.activities').children().last().remove();
+$('.activities').append('<p>$' + cost +'</p>');
+
+
+
+
+})
+
+//   /\w+\s\d{1,2}\w{2}[-]\d{1,2}\w{2}/   regex
